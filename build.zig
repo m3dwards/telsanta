bce7b775cfa5e883077b5729e54abb594904acbc
Max Edwards <youwontforgetthis@gmail.com>
2023-11-13 21:51:36 +0000

    put build.zig back to init-exe

--- build.zig
+++ build.zig
@@ -1,56 +1,56 @@
 const std = @import("std");
-const curl = @import("tgz/zigcurl/curl.zig");
-const zlib = @import("tgz/zigcurl/zlib.zig");
-const mbedtls = @import("tgz/zigcurl/mbedtls.zig");
 
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
 pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const z = zlib.create(b, target);
-    const tls = mbedtls.create(b, target);
-    const libcurl = curl.create(b, target, optimize);
-    libcurl.linkLibrary(z);
-    libcurl.linkLibrary(tls);
-
-    const tgz = b.addStaticLibrary(.{
-        .name = "tgz",
-        .root_source_file = .{ .path = "tgz/src/bot.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    tgz.addIncludePath(.{ .path = "./tgz/src" });
-    tgz.addCSourceFile(.{ .file = .{ .path = "tgz/src/jsmn.c" }, .flags = &.{"-std=c89"} });
-    tgz.installHeader("tgz/src/jsmn.h", "jsmn.h");
-    tgz.linkLibrary(libcurl);
-    b.installArtifact(tgz);
-
-    const tgzmod = b.addModule("tgz", .{
-        .source_file = .{ .path = "tgz/src/bot.zig" },
-    });
     const exe = b.addExecutable(.{
-        .name = "telsanta",
+        .name = "zig",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    exe.linkLibrary(tgz);
-    b.installArtifact(exe);
-    exe.addModule("tgz", tgzmod);
 
-    const run = b.addRunArtifact(exe);
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
     // By making the run step depend on the install step, it will be run from the
     // installation directory rather than directly from within the cache directory.
     // This is not necessary, however, if the application depends on other installed
     // files, this ensures they will be present and in the expected location.
-    run.step.dependOn(b.getInstallStep());
+    run_cmd.step.dependOn(b.getInstallStep());
+
     // This allows the user to pass arguments to the application in the build
     // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
-        run.addArgs(args);
+        run_cmd.addArgs(args);
     }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run.step);
+    run_step.dependOn(&run_cmd.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
