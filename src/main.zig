2761a54f2731efa2ba6e273ddf89cae2a9e83bab
Max Edwards <youwontforgetthis@gmail.com>
2023-11-18 20:50:56 +0000

    example http request

--- src/main.zig
+++ src/main.zig
@@ -1,45 +1,54 @@
 const std = @import("std");
-const builtin = @import("builtin");
-const Telezig = @import("telezig.zig");
 
-fn getToken(token_path: []const u8, buffer: []u8) !void {
-    const file = try std.fs.cwd().openFile(token_path, .{ .mode = .read_only });
-    defer file.close();
-    _ = try file.reader().read(buffer);
+pub fn main() !void { // our http client, this can make multiple requests (and is even threadsafe, although individual requests are not).
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    var client = std.http.Client{
+        .allocator = allocator,
+    };
+
+    // we can `catch unreachable` here because we can guarantee that this is a valid url.
+    const uri = std.Uri.parse("https://example.com") catch unreachable;
+
+    // these are the headers we'll be sending to the server
+    var headers = std.http.Headers{ .allocator = allocator };
+    defer headers.deinit();
+
+    try headers.append("accept", "*/*"); // tell the server we'll accept anything
+
+    // make the connection and set up the request
+    var req = try client.request(.GET, uri, headers, .{});
+    defer req.deinit();
+
+    // I'm making a GET request, so do I don't need this, but I'm sure someone will.
+    // req.transfer_encoding = .chunked;
+
+    // send the request and headers to the server.
+    try req.start();
+
+    // try req.writer().writeAll("Hello, World!\n");
+    // try req.finish();
+
+    // wait for the server to send use a response
+    try req.wait();
+
+    // read the content-type header from the server, or default to text/plain
+    const content_type = req.response.headers.getFirstValue("content-type") orelse "text/plain";
+    _ = content_type;
+
+    // read the entire response body, but only allow it to allocate 8kb of memory
+    const body = req.reader().readAllAlloc(allocator, 8192) catch unreachable;
+    defer allocator.free(body);
+
+    std.log.info("{s}", .{body});
 }
 
-pub fn main() !void {
-    // Windows-only startup, this might not be needed in the future if fixed, see: https://github.com/ziglang/zig/issues/8943
-    if (builtin.os.tag == std.Target.Os.Tag.windows) _ = try std.os.windows.WSAStartup(2, 0);
-
-    var allocator = std.testing.allocator;
-    var token: [46]u8 = undefined;
-    // Here we call the function above to load the token from a file,
-    // but you can decide to load it from an environment variable or any other way
-    try getToken("token.txt", token[0..]);
-    var bot = try Telezig.init(allocator, token[0..]);
-
-    var update_id: i64 = std.math.minInt(i64);
-    var sleep_seconds: u64 = 10;
-    // Here we run an infinite loop to get messages written to the bot and respond with the same text.
-    // The only way to stop it is to kill the app
-    while (true) {
-        // Sleep some seconds on each loop to not make too many requests to the Telegram API
-        std.time.sleep(sleep_seconds * std.time.ns_per_s);
-
-        // Get the updates from the Telegram API
-        var update = try bot.getUpdates();
-        defer bot.allocator.free(update.text);
-
-        // We only send an echo message if someone has sent any new message to the bot
-        var new_update_id = update.update_id;
-        if (update_id == new_update_id) continue;
-        update_id = new_update_id;
-
-        // Send the same message we received
-        try bot.sendMessage(update);
-    }
-
-    // Windows-only cleanup, this might not be needed in the future if fixed, see: https://github.com/ziglang/zig/issues/8943
-    if (builtin.os.tag == std.Target.Os.Tag.windows) try std.os.windows.WSACleanup();
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
